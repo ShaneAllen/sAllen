91bd74ac12825b0c6c8c297ecaaf2f1e9db4dc24
Shane Allen <B5003704@my.shu.ac.uk>
2019-04-23 20:26:40 +0100

    Updated models and added views to admin

--- reviewApp/reviews/models.py
+++ reviewApp/reviews/models.py
@@ -9,21 +9,29 @@ class Category(models.Model):
 	name = models.CharField(default = '', max_length = 255)
 	description = models.TextField()
 
+	def __str__(self):
+		return self.name
+
 class Product(models.Model):
 	name = models.CharField(default = '', max_length = 255)
 	brand = models.CharField(default = '', max_length = 255)
-	avg_cost = models.DecimalField(default = 00.00, max_digits = 4, decimal_places = 2)
+	avg_cost = models.DecimalField(default = 00.00, max_digits = 5, decimal_places = 2)
 	category = models.ForeignKey(Category, on_delete = models.CASCADE)
-	date_released = models.TextField()
+	date_released = models.DateField(default = timezone.now)
+	description = models.TextField(default = '')
 	photo = models.ImageField(default = 'default.jpg', upload_to = 'product_pics')
 
+	def __str__(self):
+		return self.name
 
-class review(models.Model):
+class Review(models.Model):
 	author = models.ForeignKey(User, on_delete = models.CASCADE)
 	product = models.ForeignKey(Product, on_delete = models.CASCADE)
 	rating = models.IntegerField(default = 0, max_length = 5)
-	review_text = models.TextField()
+	review_text = models.TextField(default = '')
 	date = models.DateField(default = timezone.now)
 
+	def __str__(self):
+		return f'This is a review of: {self.product.name} by {self.author.profile.first_name}'
 # class Reviews(models.Model):
 
